e0b10342387ba47971603ad27b053fc570ae3883
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-08-07 13:20:27 -0500

    setup.py: include requirements.txt

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ except ImportError:
 from pip.req import parse_requirements
 from pip.download import PipSession
 
-config = {
-    'description': 'freeze arbitrary Python things as immutables',
-    'author': 'Andrew Pennebaker',
-    'url': 'https://github.com/mcandre/liquidnitrogen',
-    'download_url': 'git@github.com:mcandre/liquidnitrogen.git',
-    'author_email': 'andrew.pennebaker@gmail.com',
-    'version': '0.0',
-    'install_requires': [
+setup(
+    description='freeze arbitrary Python things as immutables',
+    author='Andrew Pennebaker',
+    url='https://github.com/mcandre/liquidnitrogen',
+    download_url='git@github.com:mcandre/liquidnitrogen.git',
+    author_email='andrew.pennebaker@gmail.com',
+    version='0.0',
+    install_requires=[
         str(r.req)
         for r in parse_requirements('requirements.txt', session=PipSession())
     ],
-    'packages': ['liquidnitrogen'],
-    'scripts': [],
-    'name': 'liquidnitrogen',
-    'license': 'FreeBSD'
-}
-
-setup(**config)
+    packages=['liquidnitrogen'],
+    package_data={'': ['requirements.txt']},
+    include_package_data=True,
+    scripts=[],
+    name='liquidnitrogen',
+    license='FreeBSD'
+)
