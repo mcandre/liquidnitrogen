cfe4f4928759f7475a9a761591be7eba842a8cb0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 19:47:55 -0600

    update editorconfig

--- tasks.py
+++ tasks.py
@@ -3,57 +3,57 @@ from invoke import run, task
 
 @task
 def test():
-    run('nosetests')
+        run('nosetests')
 
 
 @task
 def install():
-    run('pip install -e .')
+        run('pip install -e .')
 
 
 @task
 def uninstall():
-    run('pip uninstall liquidnitrogen')
+        run('pip uninstall liquidnitrogen')
 
 
 @task
 def publish():
-    run('python setup.py register')
-    run('python setup.py sdist upload')
+        run('python setup.py register')
+        run('python setup.py sdist upload')
 
 
 @task
 def pep8():
-    run('pep8 .')
+        run('pep8 .')
 
 
 @task
 def pylint():
-    run('pylint *.py')
+        run('pylint *.py')
 
 
 @task
 def pyflakes():
-    run('pyflakes .')
+        run('pyflakes .')
 
 
 @task
 def flake8():
-    run('flake8 .')
+        run('flake8 .')
 
 
 @task
 def bandit():
-    run('find . -name \'*.py\' | xargs bandit')
+        run('find . -name \'*.py\' | xargs bandit')
 
 
 @task(pre=[pep8, pylint, pyflakes, flake8, bandit])
 def lint():
-    pass
+        pass
 
 
 @task
 def clean():
-    run('rm -rf dist/; true')
-    run('find . -type d -name __pycache__ -exec rm -rf {} \\;; true')
-    run('rm -rf *.egg-info; true')
+        run('rm -rf dist/; true')
+        run('find . -type d -name __pycache__ -exec rm -rf {} \\;; true')
+        run('rm -rf *.egg-info; true')
